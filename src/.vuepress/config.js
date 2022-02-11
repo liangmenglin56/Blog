5895bd602072aff9cde78da0efcfabe975fcd0a0
梁梦林 <liangmenglin@tuhu-lml.local>
2022-02-11 17:21:05 +0800

    新增菜单

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -6,13 +6,19 @@ module.exports = {
         nav:[ // 导航栏配置
             {text: '首页', link: '/' },
             {text: '前端基础', link: '/basic/container/a' },
-            {text: 'JavaScript', link: '/javascript/' }
+            {text: 'JavaScript', link: '/javascript/container/原型与原型链' },
+            {text: 'Vue', link: '/vue/' },
+            {text: 'CSS', link: '/css/' }
         ],
         sidebar: {
             '/basic/container/': [
                 'a',
                 'b',
                 'basic'
+            ],
+            '/javascript/container/': [
+                '原型与原型链',
+                '作用域'
             ]
         }
     }
